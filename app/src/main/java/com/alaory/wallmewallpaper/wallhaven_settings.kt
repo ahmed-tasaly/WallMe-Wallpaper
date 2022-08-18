20fe27e4a63fad4689acb1c37a9a60b73d5f9803
Alaory <doom.roll.024@gmail.com>
2022-08-18 16:42:59 +0300

    working on  tags

--- app/src/main/java/com/alaory/wallmewallpaper/wallhaven_settings.kt
+++ app/src/main/java/com/alaory/wallmewallpaper/wallhaven_settings.kt
@@ -1,11 +1,16 @@
 package com.alaory.wallmewallpaper
 
+import android.app.AlertDialog
+import android.content.DialogInterface
 import android.graphics.drawable.Drawable
 import android.os.Bundle
+import android.text.InputType
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import androidx.core.content.res.ResourcesCompat
 import com.google.android.material.chip.Chip
 import com.google.android.material.chip.ChipGroup
@@ -31,13 +36,35 @@ class wallhaven_settings : Fragment() {
         val ChipAdd: Chip = view.findViewById(R.id.AddChip_box);
 
         ChipAdd.setOnClickListener {
+            val builder = AlertDialog.Builder(requireContext())
             val tagchipTemp = LayoutInflater.from(requireContext()).inflate(R.layout.tagchip,TagBox,false) as Chip;
-            tagchipTemp.text = "Iaggg";
-            tagchipTemp.setOnClickListener {
-                TagBox.removeView(tagchipTemp);
-            }
-            tagchipTemp.chipIcon = ResourcesCompat.getDrawable(resources,R.drawable.remove_ic,null);
-            TagBox.addView(tagchipTemp);
+            val inputText = EditText(requireContext());
+
+
+
+
+            inputText.hint = "Enter a Tag name";
+            inputText.inputType = InputType.TYPE_CLASS_TEXT
+
+            builder.setView(inputText);
+            builder.setTitle("Add Tag");
+
+            builder.setPositiveButton("Add",DialogInterface.OnClickListener { dialogInterface, i ->
+                val inputStringText = inputText.text.toString();
+                if(inputStringText != ""){
+                    tagchipTemp.text = inputStringText;
+                    tagchipTemp.setOnClickListener {
+                        TagBox.removeView(tagchipTemp);
+                    }
+                    tagchipTemp.chipIcon = ResourcesCompat.getDrawable(resources,R.drawable.remove_ic,null);
+                    TagBox.addView(tagchipTemp);
+                }
+            })
+            builder.setNegativeButton("Cancel",DialogInterface.OnClickListener { dialogInterface, i ->
+                Log.i("wallhaven_settings","i am negtive");
+            })
+            builder.show();
+
         }
 
     }
