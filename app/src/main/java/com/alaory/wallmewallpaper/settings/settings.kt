cfb7c5a99ca0bf8ea23d08638ae79eb6d99ced1a
Alaory <doom.roll.024@gmail.com>
2022-11-05 10:34:46 +0200

    move wallpaper changer from settings

--- app/src/main/java/com/alaory/wallmewallpaper/settings/settings.kt
+++ app/src/main/java/com/alaory/wallmewallpaper/settings/settings.kt
@@ -27,9 +27,7 @@ class settings( menuChange : MainActivity.MenuChange? = null) : Fragment() {
 
     val MenuChange = menuChange;
     var backbutton : ImageButton? =null;
-    //wallpaper changer
-    var wallpaper_changer : LinearLayout? = null;
-    var wallpaper_changer_text : TextView? = null;
+
 
     //sources
     var wallhaven_source : SwitchMaterial? = null;
@@ -47,47 +45,13 @@ class settings( menuChange : MainActivity.MenuChange? = null) : Fragment() {
     var supportMe : TextView? = null;
 
 
-
-
-    var editbox : EditText? = null;
-    var timecount : Spinner? = null;
-    var screenFlag : Spinner? = null;
-
     val JOBID = 212;
 
-
-
-
-    companion object{
-        var time: String = "15";
-        var timecountSelection: Int = 0;
-        var screenSelection: Int = 0;
-
-        fun saveprefs(context: Context){
-            val preferenceManager = context.getSharedPreferences("settings",Context.MODE_PRIVATE);
-
-            preferenceManager.edit().putString("timecount",time).apply();
-            preferenceManager.edit().putInt("timecountSelection",timecountSelection).apply();
-            preferenceManager.edit().putInt("screenSelection",screenSelection).apply();
-        }
-        fun loadprefs(context: Context){
-            val preferenceManager = context.getSharedPreferences("settings",Context.MODE_PRIVATE);
-            time = preferenceManager.getString("timecount","15")!!.toString();
-            timecountSelection = preferenceManager.getInt("timecountSelection",0);
-            screenSelection = preferenceManager.getInt("screenSelection",0);
-        }
-
-        val WorkerTag = "WallpaperChanger";
-        var isworkerrunning = false;
-
-    }
 
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val layout = inflater.inflate(R.layout.fragment_settings, container, false);
         MenuChange?.Shownav(false);
-
-        loadprefs(requireContext());
 
         requireActivity().onBackPressedDispatcher.addCallback(this) {
             MenuChange?.ChangeTo(MainActivity.menu.wallpaperchanger,true);
@@ -95,8 +59,6 @@ class settings( menuChange : MainActivity.MenuChange? = null) : Fragment() {
 
         backbutton = layout.findViewById(R.id.backArrow_button);
 
-        wallpaper_changer = layout.findViewById(R.id.wallpaper_changer_settings_button);
-        wallpaper_changer_text = layout.findViewById(R.id.wallpaper_changer_settings_button_text);
 
         wallhaven_source = layout.findViewById(R.id.Switch_wallhaven_settings);
         reddit_source = layout.findViewById(R.id.Switch_reddit_settings);
@@ -110,60 +72,7 @@ class settings( menuChange : MainActivity.MenuChange? = null) : Fragment() {
         github = layout.findViewById(R.id.github_settings);
         supportMe = layout.findViewById(R.id.support_settings);
 
-        editbox = layout.findViewById(R.id.editTextTime);
-        timecount = layout.findViewById(R.id.wallpaper_changer_time_spinner);
-        screenFlag = layout.findViewById(R.id.wallpaper_changer_time_spinner_forScreen);
 
-        val worklistinfo = WorkManager.getInstance(requireContext()).getWorkInfosByTag(WorkerTag)
-        for (i in worklistinfo.get())
-            if(!i.state.isFinished ){
-                Log.i("workermee",i.tags.toString());
-                isworkerrunning = true;
-            }
-
-
-        if(isworkerrunning)
-            wallpaper_changer_text?.setText("Stop wallpaper changer")
-
-        wallpaper_changer?.let {
-            it.setOnClickListener {
-
-               if(!isworkerrunning) {
-                    saveprefs(requireContext());
-
-                    var localtimecount: TimeUnit = TimeUnit.MINUTES;
-                    when (timecountSelection){
-                        0 -> {
-                            localtimecount = TimeUnit.MINUTES;
-                        }
-                        1 -> {
-                            localtimecount = TimeUnit.HOURS;
-                        }
-                        2 -> {
-                            localtimecount = TimeUnit.DAYS;
-                        }
-                    }
-
-                   val wrokerConstraints = Constraints.Builder()
-                       .setRequiredNetworkType(NetworkType.CONNECTED)
-                       .build()
-
-                   val workreq = PeriodicWorkRequestBuilder<wallpaperChanger_Worker>(time.toLong(),localtimecount)
-                       .addTag(WorkerTag)
-                       .setConstraints(wrokerConstraints)
-                       .build();
-
-
-                   WorkManager.getInstance(requireContext()).enqueueUniquePeriodicWork(WorkerTag, ExistingPeriodicWorkPolicy.REPLACE,workreq);
-                   wallpaper_changer_text?.setText("Stop wallpaper changer");
-                   isworkerrunning = true;
-                }else{
-                    isworkerrunning = false;
-                   wallpaper_changer_text?.setText("Start wallpaper changer");
-                    WorkManager.getInstance(requireContext()).cancelAllWorkByTag(WorkerTag);
-                }
-            }
-        }
 
         backbutton?.let {
             it.setOnClickListener {
@@ -171,57 +80,6 @@ class settings( menuChange : MainActivity.MenuChange? = null) : Fragment() {
             }
         }
 
-        editbox?.let {
-            it.setText(time);
-            it.addTextChangedListener(object : TextWatcher{
-                override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-
-                }
-
-                override fun onTextChanged(text: CharSequence?, p1: Int, p2: Int, p3: Int) {
-                    time = text.toString();
-                }
-
-                override fun afterTextChanged(p0: Editable?) {
-                    p0?.let {
-                        if(it.isEmpty()){
-                            it.append("15")
-                        }
-                    }
-                }
-            })
-        }
-        timecount?.let {
-            it.setSelection(timecountSelection);
-            it.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
-                override fun onItemSelected(p0: AdapterView<*>?, p1: View?, pos: Int, selection: Long) {
-                    timecountSelection = selection.toInt();
-                }
-
-                override fun onNothingSelected(p0: AdapterView<*>?) {}
-            }
-        }
-
-        fullscreenapp?.let {
-            it.isChecked = requireContext().getSharedPreferences("settings",Context.MODE_PRIVATE).getBoolean("fullscreenapp",true);
-            it.setOnCheckedChangeListener { p0, ischecked ->
-                val prefs = p0.context.getSharedPreferences("settings",Context.MODE_PRIVATE);
-                prefs.edit().putBoolean("fullscreenapp",ischecked).apply();
-                wallmewallpaper.doFullscreen = ischecked;
-            }
-        }
-
-
-        screenFlag?.let {
-            it.setSelection(screenSelection);
-            it.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
-                override fun onItemSelected(p0: AdapterView<*>?, p1: View?, pos: Int, selection: Long) {
-                    screenSelection = selection.toInt();
-                }
-
-                override fun onNothingSelected(p0: AdapterView<*>?) {}
-            }
-        }
 
 
         wallhaven_source?.let {
